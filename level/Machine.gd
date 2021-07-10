3f53857f44b85978dd359f4e759583f5bc00b273
Gernot Raudner <gitkraken@gernotraudner.at>
2021-07-10 22:58:53 +0200

    BROKEN machine

--- level/Machine.gd
+++ level/Machine.gd
@@ -1,38 +1,56 @@
-extends Area2D
+extends Node2D
 
-export var processing_time := 5.0
-
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
 enum MachineState {
 	IDLE,
 	RUNNING,
 	READY,	
 }
 
+const BubbleColors = preload("res://utils/GlobalEnums.gd").BubbleColors
+
+export var processing_time := 5.0
+
 var _state = MachineState.IDLE
 var _player_was_here = false
-# Called when the node enters the scene tree for the first time.
+var _active_button = null
+var _prio_queue = []
+
+onready var colors = {
+	BubbleColors.RED: $Red,
+	BubbleColors.GREEN: $Green,
+	BubbleColors.BLUE: $Blue,
+}
+
 func _ready() -> void:
-	pass # Replace with function body.
+	for col in BubbleColors.size():
+		colors[col].connect("player_entered", self, "_player_entered")
+
+
+func _player_entered(bubble_type: int) -> void:
+	_prio_queue.push_back(bubble_type)
+	if _active_button:
+		_active_button.modulate = Color.white
+	_active_button = colors[bubble_type]
+	_active_button.modulate = Color.green
+
+
+func _player_left(bubble_type: int) -> void:
+	var id = _prio_queue.bsearch(bubble_type)
+	_prio_queue.remove(id)
+	if _active_button == colors[bubble_type]:
+		_active_button.modulate = Color.white
+	if _prio_queue.size() == 0:
+		_active_button = null
+	else:
+		var type = _prio_queue[_prio_queue.size() - 1]
+		_active_button = colors[type]
+		_active_button.modulate = Color.green
 
 
 func _physics_process(delta: float) -> void:
-	var areas = get_overlapping_areas()
-	var player_is_here = false
-	for area in areas:
-		if area is Player:
-			player_is_here = true
-			if Input.is_action_just_pressed("ui_up"):
-				_handle_action_press(area)
-	if player_is_here and not _player_was_here:
-		# enable highlight
+	if Input.is_action_just_pressed("ui_up"):
 		pass
-	if _player_was_here and not player_is_here:
-		# disable highlight
-		pass
-	_player_was_here = player_is_here
+		#_handle_action_press(area)
 
 
 func _handle_action_press(player: Player) -> void:
---
+++ level/MachineButton.gd
@@ -0,0 +1,23 @@
+extends Area2D
+class_name MachineButton
+
+signal player_entered
+signal player_left
+
+const BubbleColors = preload("res://utils/GlobalEnums.gd").BubbleColors
+
+export var bubble_color := BubbleColors.RED
+
+var _player_was_here
+
+func _physics_process(delta: float) -> void:
+	var areas = get_overlapping_areas()
+	var player_is_here = false
+	for area in areas:
+		if area is Player:
+			player_is_here = true
+	if player_is_here and not _player_was_here:
+		emit_signal("player_entered", bubble_color)
+	if _player_was_here and not player_is_here:
+		emit_signal("player_left", bubble_color)
+	_player_was_here = player_is_here
